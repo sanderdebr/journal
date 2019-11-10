862b9dc9d791a1c693e6a21984a93e767d119b08
sanderdebr <sander606@gmail.com>
2019-11-10 11:16:41 +0100

    'update'

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from 'gatsby'
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Nav from '../components/nav';
+import Featured from '../components/featured';
+import Home from "../components/home";
+import './index.css'
+import Footer from '../components/footer';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Nav />
+    <Featured />
+    <Home />
+    <Link to='./blog' className='viewmore'>View More</Link>
+    <Footer />
+  </Layout>
+)
+
+export default IndexPage
